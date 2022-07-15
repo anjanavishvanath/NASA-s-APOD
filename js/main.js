6b28544de38e630562cd64a4b02098ccd8743795
“anjanavishvanath” <anjanavishvanath3@gmail.com>
2022-07-15 20:54:20 +0530

    Select any date to get APOD

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
-fetch("https://api.nasa.gov/planetary/apod?api_key=4RfRckfBr273HjxoOlMnSPbzwqLanvJaQC559f2d")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data.date)
-        console.log(data.explanation)
-        console.log(data.hdurl)
-        console.log(data.url)
-        console.log(data.title)
-        document.querySelector('.title-of-image').innerHTML = data.title
-        document.querySelector('img').src = data.url
-        document.querySelector('.date').innerHTML = data.date
-        document.querySelector('.description').innerHTML = data.explanation
-    })
-    .catch(err => console.log(`error ${err}`))
+document.querySelector('button').addEventListener('click',getData)
+
+function getData(){
+    let date = document.querySelector('input').value
+    console.log(date)
+
+    fetch(`https://api.nasa.gov/planetary/apod?api_key=4RfRckfBr273HjxoOlMnSPbzwqLanvJaQC559f2d&date=${date}`)
+        .then(res => res.json())
+        .then(data => {
+            document.querySelector('.title-of-image').innerHTML = data.title
+            document.querySelector('img').src = data.url
+            document.querySelector('.date').innerHTML = data.date
+            document.querySelector('.description').innerHTML = data.explanation
+        })
+        .catch(err => console.log(`error ${err}`))
+}
